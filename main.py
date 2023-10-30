a799e9b9738edbab6ec67d97c948bddf53e8da8b
QuantuM410 <kartikeys410@gmail.com>
2023-10-30 21:28:37 +0530

    updated main.py with test.py inference

--- main.py
+++ main.py
@@ -1,34 +1,66 @@
 from ultralytics import YOLO
-import supervision as sv
-import numpy as np
+from supervision.utils.video import get_video_frames_generator, VideoInfo, VideoSink
+from supervision.detection.core import Detections #gives detection object with xyxy, confidence, class_id attributes for each detection in the frame'
+from supervision.annotators.core import BoundingBoxAnnotator 
+from supervision.tracker.byte_tracker.core import ByteTrack
+from supervision.geometry.core import Point
+from supervision.annotators.core import LabelAnnotator
+from supervision.detection.line_counter import LineZone, LineZoneAnnotator
+
+model = YOLO("yolov8m.pt")
+model.fuse()
+
+SOURCE_VIDEO_PATH = "people-walking.mp4"
+TARGET_VIDEO_PATH = "people-walking-result.mp4"
+video_info = VideoInfo.from_video_path(SOURCE_VIDEO_PATH) #returns video informations such as its size, total frames, fps
+
+LINE_START = Point(0, video_info.height // 2) #pierces through the center of the window
+LINE_END = Point(video_info.width, video_info.height // 2)
+
+tracker = ByteTrack()
+
+generator = get_video_frames_generator(SOURCE_VIDEO_PATH)
+
+line_counter = LineZone(start=LINE_START, end=LINE_END)
+
+box_annotator = BoundingBoxAnnotator(thickness=4)   
+
+label_annotator = LabelAnnotator()
+
+line_annotator = LineZoneAnnotator(text_scale=2, thickness=4)
+
+CLASS_NAMES_DICT = model.model.names
+
+CLASS_ID = [0]
+
+with VideoSink(TARGET_VIDEO_PATH, video_info) as sink:
+    for frame in generator: 
+        '''
+        tqdm is a progress bar library with good support for nested loops and Jupyter/IPython notebooks.
+        frame gives an ndarray numpy array of frame in the give video
+        '''
+
+        results = model(frame)[0]
+
+        detections = Detections.from_ultralytics(results)#supervision uses Detection type objects
+        detections = detections[(detections.class_id == 0) & (detections.confidence > 0.5)]
+        detections = tracker.update_with_detections(detections)
+
+        labels = [
+            f"#{tracker_id} {results.names[class_id]}"
+            for class_id, tracker_id
+            in zip(detections.class_id, detections.tracker_id)
+        ]
+        
+        line_counter.trigger(detections=detections) #updates the line counter with the detections in the frame
+
+        frame = box_annotator.annotate(scene=frame, detections=detections)
+
+        line_annotator.annotate(frame=frame, line_counter=line_counter)
+
+        annotated_frame = box_annotator.annotate(frame.copy(), detections=detections)
+        label_annotator.annotate(annotated_frame, detections=detections, labels=labels)
+
+        sink.write_frame(annotated_frame) #write each predicted frame to the target video path
 
 
-model = YOLO("yolov8m.pt") 
-tracker = sv.ByteTrack()
-box_annotator = sv.BoundingBoxAnnotator()
-label_annotator = sv.LabelAnnotator()
-
-def callback(frame: np.ndarray, _: int) -> np.ndarray:
-    results = model(frame)[0]
-    detections = sv.Detections.from_ultralytics(results)
-    detections = detections[(detections.class_id == 0) & (detections.confidence > 0.5)]
-    detections = tracker.update_with_detections(detections)
-
-    labels = [
-        f"#{tracker_id} {results.names[class_id]}"
-        for class_id, tracker_id
-        in zip(detections.class_id, detections.tracker_id)
-    ]
-
-    annotated_frame = box_annotator.annotate(
-        frame.copy(), detections=detections)
-    return label_annotator.annotate(
-        annotated_frame, detections=detections, labels=labels)
-
-video_info = sv.VideoInfo.from_video_path('people-walking.mp4')
-frames_generator = sv.get_video_frames_generator('people-walking.mp4')
-sv.process_video(
-    source_path="people-walking.mp4",
-    target_path="result.mp4",
-    callback=callback,
-)
