44deb698b62aa8ad6a9faa5a6b026ff1cb433906
QuantuM410 <kartikeys410@gmail.com>
2023-10-30 23:23:57 +0530

    Added another inference for zone detection and count in the footage and updated some existing comments

--- main.py
+++ main.py
@@ -1,61 +1,74 @@
 from ultralytics import YOLO
+import numpy as np  
 from supervision.utils.video import get_video_frames_generator, VideoInfo, VideoSink
 from supervision.detection.core import Detections
 from supervision.tracker.byte_tracker.core import ByteTrack
 from supervision.geometry.core import Point
 from supervision.annotators.core import BoundingBoxAnnotator, LabelAnnotator
-from supervision.detection.line_counter import LineZone, LineZoneAnnotator
+from supervision.detection.tools.polygon_zone import PolygonZone, PolygonZoneAnnotator
+from supervision.draw.color import Color
 
-def process_video(input_video_path, output_video_path, yolo_model_path):
-    # Load YOLO model and fuse it
-    yolo_model = YOLO(yolo_model_path)
-    yolo_model.fuse()
+class CCTVCounter():
 
-    # Get video information
-    video_info = VideoInfo.from_video_path(input_video_path)
+    def process_video(input_video_path, output_video_path, yolo_model_path):
+        # Loads YOLO model and fuse it
+        yolo_model = YOLO(yolo_model_path)
+        yolo_model.fuse()
 
-    # Define the line parameters
-    LINE_START = Point(0, video_info.height // 2)
-    LINE_END = Point(video_info.width, video_info.height // 2)
+        # Gets video information
+        video_info = VideoInfo.from_video_path(input_video_path)
 
-    # Initialize the tracker
-    tracker = ByteTrack()
+        polygon_map = np.array([[0,video_info.height],[video_info.width,video_info.height],[video_info.width,0],[0,0]])
 
-    # Get video frame generator
-    frame_generator = get_video_frames_generator(input_video_path)
+        # Defining the polygon parameters
+        zone = PolygonZone(polygon=polygon_map, frame_resolution_wh=video_info.resolution_wh)
 
-    # Initialize the line counter
-    line_counter = LineZone(start=LINE_START, end=LINE_END)
+        # Initialize the tracker
+        tracker = ByteTrack()
 
-    # Initialize annotators
-    box_annotator = BoundingBoxAnnotator(thickness=4)
-    label_annotator = LabelAnnotator()
-    line_annotator = LineZoneAnnotator(text_scale=2, thickness=4)
+        # Gets video frame generator
+        frame_generator = get_video_frames_generator(input_video_path)
 
-    with VideoSink(output_video_path, video_info) as sink:
-        for frame in frame_generator:
-            results = yolo_model(frame)[0]
+        # Initializes annotators
+        box_annotator = BoundingBoxAnnotator(thickness=4)
+        zone_annotator = PolygonZoneAnnotator(zone=zone, color=Color.white(), thickness=4, text_scale=4, text_thickness=4)
+        label_annotator = LabelAnnotator()
 
-            detections = Detections.from_ultralytics(results)
-            detections = detections[(detections.class_id == 0) & (detections.confidence > 0.5)]
-            detections = tracker.update_with_detections(detections)
+        total_count = 0  # Variable to keep track of the total count
 
-            labels = [f"#{tracker_id} {results.names[class_id]}" for class_id, tracker_id in zip(detections.class_id, detections.tracker_id)]
-            
-            # Update the line counter
-            line_counter.trigger(detections=detections)
+        with VideoSink(output_video_path, video_info) as sink:
+            for frame in frame_generator:
+                results = yolo_model(frame)[0]
 
-            # Annotate the frame
-            annotated_frame = box_annotator.annotate(frame.copy(), detections=detections)
-            label_annotator.annotate(annotated_frame, detections=detections, labels=labels)
-            line_annotator.annotate(annotated_frame, line_counter=line_counter)
+                detections = Detections.from_ultralytics(results)
+                detections = detections[(detections.class_id == 0) & (detections.confidence > 0.5)]
 
-            # Write the annotated frame to the output video
-            sink.write_frame(annotated_frame)
+                #triggers zone on each frame and display the objects inside the zone
+                zone.trigger(detections=detections)
+
+                #updates the tracker for each object in the zone
+                detections = tracker.update_with_detections(detections)
+
+                labels = [f"#{tracker_id} {results.names[class_id]}" for class_id, tracker_id in zip(detections.class_id, detections.tracker_id)]
+                
+                # Annotates the frame
+                frame = box_annotator.annotate(scene=frame, detections=detections)
+                frame = zone_annotator.annotate(scene=frame)
+                label_annotator.annotate(scene=frame, detections=detections, labels=labels)
+
+                # Counting the number of objects in the zone
+                total_count += len(detections)
+
+                # Writes the annotated frame to the output video
+                sink.write_frame(frame)
+        
+        average_count = total_count // video_info.total_frames
+        
+        return average_count
 
 if __name__ == "__main__":
     SOURCE_VIDEO_PATH = "people-walking.mp4"
-    TARGET_VIDEO_PATH = "people-walking-result.mp4"
+    TARGET_VIDEO_PATH = "people-walking-zone-result.mp4"
     YOLO_MODEL_PATH = "yolov8m.pt"
 
-    process_video(SOURCE_VIDEO_PATH, TARGET_VIDEO_PATH, YOLO_MODEL_PATH)
+    CCTVCounter.process_video(SOURCE_VIDEO_PATH, TARGET_VIDEO_PATH, YOLO_MODEL_PATH)
